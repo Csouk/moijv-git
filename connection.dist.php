d2ebd3986024d25aaa20d8c6be4cd16662cc1273
Csouk <kevin.cessac@laposte.net>
2017-12-14 16:38:50 +0100

    Add a formular to register a new game

--- connection.dist.php
+++ connection.dist.php
@@ -1,3 +1,15 @@
 <?php
 	$connexion = new PDO('mysql:host=localhost;dbname=moijv;charset=utf8', 'root', '');
+	
+	//--------------------------------- SESSION ------------------------------------
+	session_start();
+
+	//---------------------------------- CHEMIN ------------------------------------
+	define("RACINE_SITE", $_SERVER['DOCUMENT_ROOT'] . "/PHP/boutique/");
+	// echo RACINE_SITE;
+
+	define("URL", 'http://localhost/PHP/moijv-git/');
+
+	//--------------------------------- VARIABLE -----------------------------------
+	$content = "";
 ?>
